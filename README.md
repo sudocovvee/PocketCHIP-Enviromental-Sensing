3070faaf85b28641a04b82442c9221a0695d19db
sudocovvee <ibrooks1327@gmail.com>
2020-10-28 00:48:36 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PocketCHIP-Enviromental-Sensor
+ A python implementation of the CCS811 and DHT11 sensor on the PockerCHIP
---
+++ read_air_quality.py
@@ -0,0 +1,23 @@
+#import time
+from smbus2 import SMBus
+# This needs to be the Pocketchip vesion
+# import RPi.GPIO as GPIO
+import CHIP_IO.GPIO as GPIO
+
+address = 0x5a
+
+# initialize GPIO
+GPIO.setup("CSID0", GPIO.OUT)
+
+def alg_result_data():
+    with SMBus(2) as bus:
+        dat = bus.read_byte_data(address, 2)
+    return dat
+
+
+# dht11 Sensor Read
+
+#Print the data:
+
+dat = alg_result_data()
+print(dat)
