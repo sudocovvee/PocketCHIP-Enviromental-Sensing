ae40f7d3c22918a3c37eef7f2c3f8a4f4119867e
sudocovvee <ibrooks1327@gmail.com>
2020-10-31 21:04:47 -0600

    Update read_air_quality.py

--- read_air_quality.py
+++ read_air_quality.py
@@ -12,7 +12,7 @@ address = 0x5a
 def alg_result_data():
     with SMBus(2) as bus:
         eCO2 = bus.read_byte_data(address, 0x02)
-        VOC = eCO2 << 2
+        VOC = eCO2 << 4
         final_dat = ("eCO2: "+ str(eCO2) + "\nVOC: " + str(VOC)) # + dat2
     return final_dat
 
@@ -20,7 +20,7 @@ def alg_result_data():
 def status():
     with SMBus(2) as bus:
         dat1 = bus.read_byte_data(address, 0x00)
-        final_dat = str(dat1 << 7)
+        final_dat = str(dat1)
     return final_dat
 
 
